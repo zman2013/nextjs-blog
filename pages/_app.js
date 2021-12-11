bf993f7e89babd5553e1fd26c5c6feb2c4334303
zman <manzhiyuan@lixiang.com>
2021-12-11 22:54:46 +0800

    add server

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.css'
 
+import * as net from 'net'
+
+net.createServer((socket)=>{
+  console.log('new socket')
+}).listen(8080)
+
 export default function App({Component, pageProps}){
   return <Component {...pageProps} />
 }
