58df2db6b1132dc28dc8c3866fb65dc0158442a0
zman <manzhiyuan@lixiang.com>
2021-12-22 19:11:38 +0800

    update

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,8 +1,8 @@
 import * as net from 'net'
 import { startProxy } from './remote-proxy'
 
-const port = Number(process.env.proxy_port!)
-const host = process.env.proxy_host!
+const port = Number(process.env.proxy_port)
+const host = process.env.proxy_host
 
 export default function handler(req, res) {
     startProxy(message=>{
@@ -13,13 +13,13 @@ export default function handler(req, res) {
 
 
 /**
-length 
+length
 {
   cmd: CONNECT,
   host,
   port
  }
- 
+
  
  */
 
